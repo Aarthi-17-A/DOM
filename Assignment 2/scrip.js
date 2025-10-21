43142abe72743c4becf82e3ba5b18372f00682fa
Aarthi-17-A <aarthivonki@gmail.com>
2025-10-21 20:13:29 +0530

    Add files via upload

---
+++ Assignment 2/scrip.js
@@ -0,0 +1,18 @@
+// function checkDate(userTime){
+//   let currentTime = new Date().toString();
+//     if(userTime < currentTime){
+//         console.log('the time is backdated , pls enter a valid date')
+//     }else {
+//        console.log('valid date')
+//     }  
+// }
+// console.log(new Date().toString());
+// checkDate('Thu Jul 10 2025 14:57:18 GMT+0530 (India Standard Time)');
+
+// let now = new Date(); //gets the current date/time
+//  let timeIn12HourFormat = now.toLocaleTimeString(); //convetsto 12hour time format using toLocaleTimeString()  in a human readable format , en-us makes sure its in 12-hour format
+//  console.log("Current time in 12-hour format:",timeIn12HourFormat);
+
+// const birthday = new Date('2006-4-17');
+// console.log(birthday.getDay());
+
