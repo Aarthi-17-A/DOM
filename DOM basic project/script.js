072cc57a0dd90d41a0e0bb98fd7db017c634dd81
Aarthi-17-A <aarthivonki@gmail.com>
2025-10-21 21:03:01 +0530

    Add files via upload

---
+++ DOM basic project/script.js
@@ -0,0 +1,81 @@
+// let count = 0;
+// for(let i=0; i<5; i++){
+//     let temp ='';
+//     for(let j=0;j<5; j++){
+//         count++
+//         temp=temp+count+' ';
+//     }
+//     console.log(temp);
+// }
+
+
+
+// let now = new Date();
+// console.log(now);  //2025-07-08T16:08:57.303Z //utc
+
+// let now = new Date().toString();
+// console.log(now);  //Tue Jul 08 2025 21:40:13 GMT+0530 (India Standard Time)
+
+// let utcTime = new Date('Tue Jul 08 2025 21:15:17 GMT+0530 (India Standard Time')
+// console.log(utcTime);  //2025-07-08T15:45:17.000Z
+
+//let now=+new Date();
+//console.log(now);  //1751991139929 //timestamp
+
+
+
+//1 sec- 1000ms
+//minuts = 60 * 1000 ms
+//1 hr = 60*60*1000ms
+//1 day = 24*60*60*1000ms
+
+//console.log(now/(24 * 60 * 60 * 1000));  //20277.675784953703
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let temp = '';
+// for(let i=1; i<=5; i++){
+//     temp+=i+' ';
+// }
+// console.log(temp); // 1 2 3 4 5 
+
+//  let count = 0;
+// for(let i=0; i<5; i++){
+//     for(let j=0;j<5; j++){
+//         count++
+//         console.log(count);
+//     }
+// }
+
+
+//   let count = 0;
+// let temp ='';
+// for(let i=0; i<5; i++){
+//     for(let j=0;j<5; j++){
+//         count++
+//         temp=temp+count+' ';
+//     }
+//     console.log(temp);
+// }
+
